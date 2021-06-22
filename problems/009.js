769bbe9b0b9a51e272a93f0c6d678aed42a3aa79
sam may <spmay523@gmail.com>
2021-06-21 23:55:08 -0400

    problem 9 solved

--- problems/009.js
+++ problems/009.js
@@ -4,25 +4,69 @@
 */
 
 function isPerfectSquare(num) {
-    
+    let root = Math.sqrt(num);
+    let root_string = String(root);
+    if (root_string.indexOf('.') == -1) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-function getPythagoreanTriplet(c) {
+function checkPythagTrip(a, b, c) {
+    return( (a**2 + b**2) === c**2 );
+}
+
+function getPythagTrips(c) {
     let a = 0;
     let b = 0;
 
     let i = c;
+    i--;
 
+    let trips = [];
+
+    while (i > c/2) {
+        let a_sq = c**2 - i**2;
+        if (isPerfectSquare(a_sq)) {
+            a = Math.sqrt(a_sq);
+            trips.push({
+                "a": a,
+                "b": i,
+                "c": c
+            });
+        }
+        i--;
+    }
+
+    let success_val = trips.length > 0;
 
     return({
-        a,
-        b,
-        c
+        "success": success_val,
+        "triplets": trips
     });
 }
 
 function run() {
-
+    let sum_limit = 1000;
+    let i = sum_limit - 3;
+    while (i > 3) {
+        let trips = getPythagTrips(i);
+        if (trips.success) {
+            let trips_num = trips.triplets.length;
+            let j = 0;
+            while (j < trips_num) {
+                let this_trip = trips.triplets[j];
+                let this_sum = this_trip.a + this_trip.b + this_trip.c;
+                if (this_sum === sum_limit) {
+                    return(this_trip.a * this_trip.b * this_trip.c);
+                }
+                j++;
+            }
+        }
+        i--;
+    }
+    return 0;
 }
 
-module.exports = {run, getPythagoreanTriplet};
+module.exports = {run, getPythagTrips, isPerfectSquare, checkPythagTrip};
--- test/test.009.js
+++ test/test.009.js
@@ -1,11 +1,27 @@
 const assert = require('assert');
 const solution = require('../problems/009');
 
-describe('getPythagoreanTriplet', function() {
+describe('isPerfectSquare', function() {
+    it('should return return true for 25', function() {
+        assert.ok(solution.isPerfectSquare(25));
+    });
+
+    it('should return return false for 10', function() {
+        assert.rejects(solution.isPerfectSquare(10));
+    });
+});
+
+describe('checkPythagTrip', function() {
+    it('should return true for 3, 4, 5', function() {
+        assert.ok(solution.checkPythagTrip(3,4,5));
+    });
+});
+
+describe('getPythagTrips', function() {
     it('should find 3 and 4 to accompany 5 for pythagorean triplet', function() {
-        let answer = solution.getPythagoreanTriplet(5);
+        let answer = solution.getPythagTrips(5).triplets[0];
         assert.strictEqual(answer.a, 3);
         assert.strictEqual(answer.b, 4);
         assert.strictEqual(answer.c, 5);
-    })
+    });
 });
